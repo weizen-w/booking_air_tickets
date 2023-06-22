174e0acce22fa68e733f81377f3f9428fc7391e5
Wladimir <weizen.w@gmx.de>
2023-06-22 22:35:16 +0200

    fix: fix path to client

--- src/service/FilePath.java
+++ src/service/FilePath.java
@@ -4,8 +4,9 @@ import java.io.File;
 
 public class FilePath {
 
-  public static final String RES_FOLDER = "res";
-  public static final String CLIENTS_FOLDER = RES_FOLDER + "/clients";
-  public static final File FILE_CLIENTS = new File(CLIENTS_FOLDER + "/loginsClients.csv");
+  public static final String RES_FOLDER = "res/";
+  public static final String CLIENTS_FOLDER = RES_FOLDER + "clients/";
+  public static final File FILE_CLIENTS = new File(CLIENTS_FOLDER + "loginsClients.csv");
   public static final File PERSONAL_INFO = new File("/personalInfo.csv");
+  public static final File FILE_COUNTRIES = new File("/personalInfo.csv");
 }
