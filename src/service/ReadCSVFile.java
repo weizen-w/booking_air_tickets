b17caeed203e5df04addd503c692fb87e2ee6215
Wladimir <weizen.w@gmx.de>
2023-06-22 20:37:38 +0200

    feat: add class ReadCSVFile and WriteCSVFile

--- src/service/ReadCSVFile.java
+++ src/service/ReadCSVFile.java
@@ -1,5 +1,30 @@
 package service;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class ReadCSVFile {
 
+  public static final String SEPARATOR = ";";
+
+  public static Map<String, String> readLoginsClients() throws FileNotFoundException {
+    if (!FilePath.FILE_CLIENTS.exists()) {
+      throw new FileNotFoundException(
+          "Файл не найден или указан не верный путь" + FilePath.FILE_CLIENTS);
+    }
+    Map<String, String> map = new HashMap<>();
+    Scanner sc = new Scanner(new FileReader(FilePath.FILE_CLIENTS));
+    while (sc.hasNextLine()) {
+      String line = sc.nextLine();
+      int indexSep = line.indexOf(SEPARATOR);
+      String login = line.substring(0, indexSep);
+      String password = line.substring(indexSep + 1); // не включая разделитель
+      map.put(login, password);
+    }
+    sc.close();
+    return map;
+  }
 }
--- src/service/WriteCSVFile.java
+++ src/service/WriteCSVFile.java
@@ -1,5 +1,32 @@
 package service;
 
+import database.Client;
+import database.Database;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
 public class WriteCSVFile {
 
+  public static void writeLoginToClients() throws IOException {
+    if (Database.loginMap.isEmpty()) {
+      throw new RuntimeException("Отсутствуют данные для записи");
+    }
+    Writer writer = new FileWriter(FilePath.FILE_CLIENTS);
+    for (String keyLogin : Database.loginMap.keySet()) {
+      Client client = new Client(keyLogin, Database.loginMap.get(keyLogin));
+      writer.write(client.stringToWriteCSV() + "\n");
+    }
+    writer.close();
+  }
+
+  public static void writePersonalInfoClient(File file, List<String> list) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (String str : list) {
+      writer.write(str + "\n");
+    }
+    writer.close();
+  }
 }
