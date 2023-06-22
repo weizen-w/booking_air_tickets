0a8613bebf3769b2586a234495e69c37606cca17
Wladimir <weizen.w@gmx.de>
2023-06-22 20:36:48 +0200

    feat: add Database.loginMap

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,5 +1,18 @@
 package database;
 
+import java.io.IOException;
+import java.util.Map;
+import service.ReadCSVFile;
+
 public class Database {
 
+  public static Map<String, String> loginMap;
+
+  static {
+    try {
+      loginMap = ReadCSVFile.readLoginsClients();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
