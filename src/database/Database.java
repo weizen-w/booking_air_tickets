08e8d518f2d47b8a974d752396d8c7ed2587686a
Wladimir <weizen.w@gmx.de>
2023-06-23 02:23:56 +0200

    fix: change work with exceptions

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,18 +1,11 @@
 package database;
 
-import java.io.IOException;
 import java.util.Map;
+import service.FilePath;
 import service.ReadCSVFile;
 
 public class Database {
 
-  public static Map<String, String> loginMap;
-
-  static {
-    try {
-      loginMap = ReadCSVFile.readLoginsClients();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
+  public static Map<String, String> loginMap = ReadCSVFile.toMap(FilePath.FILE_CLIENTS);
+  public static Map<String, String> countryMap = ReadCSVFile.toMap(FilePath.FILE_COUNTRIES);
 }
