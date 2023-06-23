1b5cf7d0610658ef487d754ce8761aead2c64078
Wladimir <weizen.w@gmx.de>
2023-06-23 02:22:52 +0200

    feat: fill class Airport and Flight

--- src/aviation/Airport.java
+++ src/aviation/Airport.java
@@ -1,5 +1,31 @@
 package aviation;
 
+import java.io.File;
+import java.util.Map;
+import java.util.Scanner;
+import location.City;
+import menu.MenuMethods;
+import service.FilePath;
+import service.ReadCSVFile;
+
 public class Airport {
 
+  public final String code;
+  public final City city;
+  public final String airportFolder;
+
+  public Map<String, String> flightMap;
+
+  public Airport(String code, City city) {
+    this.code = code;
+    this.city = city;
+    airportFolder = city.cityFolder + code + "/";
+    File filePathToFlights = new File(airportFolder + FilePath.FILE_FLIGHTS);
+    flightMap = ReadCSVFile.toMap(filePathToFlights);
+  }
+
+  public static Airport select(City city, Scanner sc) {
+    String choice = MenuMethods.selectMenu(city.airportList, sc);
+    return new Airport(city.airportList.get(choice), city);
+  }
 }
--- src/aviation/Flight.java
+++ src/aviation/Flight.java
@@ -1,5 +1,45 @@
 package aviation;
 
+import database.Reservation;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 public class Flight {
 
+  private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+  private final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+  public final String number;
+  public final String cityDeparture;
+  public final String cityArrival;
+  public Airplane airplane;
+  public Date date = new Date();
+  public Date timeDeparture;
+  public Date timeArrival;
+  public Date timeInFly;
+
+  public List<Reservation> reservationList;
+
+  public Flight(String number, String cityDeparture, String cityArrival, Airplane airplane,
+      String timeDeparture, String timeArrival) throws ParseException {
+    this.number = number;
+    this.cityDeparture = cityDeparture;
+    this.cityArrival = cityArrival;
+    this.airplane = airplane;
+    this.timeDeparture = timeFormat.parse(timeDeparture);
+    this.timeArrival = timeFormat.parse(timeArrival);
+    timeInFly = new Date(this.timeArrival.getTime() - this.timeDeparture.getTime());
+  }
+
+  @Override
+  public String toString() {
+    return "number='" + number + '\'' +
+        ", cityDeparture='" + cityDeparture + '\'' +
+        ", cityArrival='" + cityArrival + '\'' +
+        ", airbus=" + airplane +
+        ", timeDeparture=" + timeDeparture +
+        ", timeArrival=" + timeArrival +
+        '}';
+  }
 }
