e3f328f261358af6a1e3091e6090e66b873dbd81
Wladimir <weizen.w@gmx.de>
2023-06-23 02:18:10 +0200

    feat: add new constants

--- src/service/FilePath.java
+++ src/service/FilePath.java
@@ -8,5 +8,9 @@ public class FilePath {
   public static final String CLIENTS_FOLDER = RES_FOLDER + "clients/";
   public static final File FILE_CLIENTS = new File(CLIENTS_FOLDER + "loginsClients.csv");
   public static final File PERSONAL_INFO = new File("/personalInfo.csv");
-  public static final File FILE_COUNTRIES = new File("/personalInfo.csv");
+  public static final String LOCATION_FOLDER = RES_FOLDER + "location/";
+  public static final File FILE_COUNTRIES = new File(LOCATION_FOLDER + "countries.csv");
+  public static final File FILE_CITIES = new File("/cities.csv");
+  public static final File FILE_AIRPORTS = new File("/airports.csv");
+  public static final File FILE_FLIGHTS = new File("/flights.csv");
 }
