b375e81d60ed5583c8ee27b44c5045b515161f94
Wladimir <weizen.w@gmx.de>
2023-06-23 02:21:42 +0200

    feat: fill class City and Country

--- src/location/City.java
+++ src/location/City.java
@@ -1,5 +1,30 @@
 package location;
 
+import java.io.File;
+import java.util.Map;
+import java.util.Scanner;
+import menu.MenuMethods;
+import service.FilePath;
+import service.ReadCSVFile;
+
 public class City {
 
+  public final String title;
+  public final Country country;
+  public final String cityFolder;
+
+  public Map<String, String> airportList;
+
+  public City(String title, Country country) {
+    this.title = title;
+    this.country = country;
+    cityFolder = country.countryFolder + title + "/";
+    File filePathToAirport = new File(cityFolder + FilePath.FILE_AIRPORTS);
+    airportList = ReadCSVFile.toMap(filePathToAirport);
+  }
+
+  public static City select(Country country, Scanner sc) {
+    String choice = MenuMethods.selectMenu(country.cityMap, sc);
+    return new City(country.cityMap.get(choice), country);
+  }
 }
--- src/location/Country.java
+++ src/location/Country.java
@@ -1,5 +1,29 @@
 package location;
 
+import database.Database;
+import java.io.File;
+import java.util.Map;
+import java.util.Scanner;
+import menu.MenuMethods;
+import service.FilePath;
+import service.ReadCSVFile;
+
 public class Country {
 
+  public final String title;
+  public final String countryFolder;
+
+  public Map<String, String> cityMap;
+
+  public Country(String title) {
+    this.title = title;
+    countryFolder = FilePath.LOCATION_FOLDER + title + "/";
+    File filePathToCities = new File(countryFolder + FilePath.FILE_CITIES);
+    cityMap = ReadCSVFile.toMap(filePathToCities);
+  }
+
+  public static Country select(Scanner sc) {
+    String choice = MenuMethods.selectMenu(Database.countryMap, sc);
+    return new Country(Database.countryMap.get(choice));
+  }
 }
