1aff8af88ed6a3c659721ac4733e1540face04c5
Wladimir <weizen.w@gmx.de>
2023-06-23 11:56:05 +0200

    feat: fill class Reservation

--- src/database/Reservation.java
+++ src/database/Reservation.java
@@ -2,4 +2,40 @@ package database;
 
 public class Reservation {
 
+  public static final String SEPARATOR = ";";
+
+  private static int numberCounter = 1;
+  public String number;
+  public final String clientLogin;
+  public int amountPassenger;
+  public double priceTotal;
+
+  public Reservation(String clientLogin, int amountPassenger) {
+    this.clientLogin = clientLogin;
+    this.amountPassenger = amountPassenger;
+    number = clientLogin + numberCounter;
+    ++numberCounter;
+  }
+
+  public Reservation(String number, String clientLogin, int amountPassenger, double priceTotal) {
+    this.number = number;
+    ++numberCounter;
+    this.clientLogin = clientLogin;
+    this.amountPassenger = amountPassenger;
+    this.priceTotal = priceTotal;
+  }
+
+  @Override
+  public String toString() {
+    return "Reservation{" +
+        "number='" + number + '\'' +
+        ", clientLogin='" + clientLogin + '\'' +
+        ", amountPassenger=" + amountPassenger +
+        ", priceTotal=" + priceTotal +
+        '}';
+  }
+
+  public String toWriteCSV() {
+    return number + SEPARATOR + clientLogin + SEPARATOR + amountPassenger + SEPARATOR + priceTotal;
+  }
 }
