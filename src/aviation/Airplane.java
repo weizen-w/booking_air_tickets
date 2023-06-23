d8a7963df9fcd6890ba90d4c5717a666c40c84bd
Wladimir <weizen.w@gmx.de>
2023-06-23 11:54:16 +0200

    feat: fill class Airplane

--- src/aviation/Airplane.java
+++ src/aviation/Airplane.java
@@ -1,5 +1,21 @@
 package aviation;
 
+
 public class Airplane {
 
+  public final String number;
+  public final int seats = 220;
+  public int seatFree = 220;
+  public double price = 100.00;
+  public boolean full = false;
+  public boolean inFly = false;
+
+  public Airplane(String number) {
+    this.number = number;
+  }
+
+  @Override
+  public String toString() {
+    return "number= " + number;
+  }
 }
